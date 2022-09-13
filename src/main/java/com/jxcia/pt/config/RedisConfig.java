4eadf1a3e95d63d62877a6cd56d6a319322ad2cd
liujie <liujie@163.com>
2022-09-13 15:39:52 +0800

    完成redis工具类编写

---
+++ src/main/java/com/jxcia/pt/config/RedisConfig.java
@@ -0,0 +1,73 @@
+package com.jxcia.pt.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(factory);
+
+        // 使用Jackson序列化器
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        // ObjectMapper
+        ObjectMapper objectMapper = new ObjectMapper();
+        // 指定要序列化的域
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // 指定序列化输入的类型
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        // 值采用json序列化
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        // 序列化key
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        // 设置hash key和value的序列化模式
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public HashOperations<String, String, Object> hashOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForHash();
+    }
+
+    @Bean
+    public ValueOperations<String, Object> valueOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForValue();
+    }
+
+    @Bean
+    public ListOperations<String, Object> listOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForList();
+    }
+
+    @Bean
+    public SetOperations<String, Object> setOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForSet();
+    }
+
+    @Bean
+    public ZSetOperations<String, Object> zSetOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForZSet();
+    }
+
+}
---
+++ src/main/java/com/jxcia/pt/utils/RedisUtil.java
@@ -0,0 +1,139 @@
+package com.jxcia.pt.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RedisUtil {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * 指定缓存失效时间
+     */
+    public void expire(String key, Long time) throws Exception {
+        if (time < 0) {
+            throw new RuntimeException("time必须大于等于0");
+        }
+        redisTemplate.expire(key, time, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 获取过期时间
+     */
+    public Long getExpire(String key) throws Exception {
+        return redisTemplate.getExpire(key, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 判断key是否存在
+     */
+    public Boolean hasKey(String key) throws Exception {
+        return redisTemplate.hasKey(key);
+    }
+
+    /**
+     * 根据key删除
+     */
+    public void del(String... keys) throws Exception {
+        if (keys != null && keys.length > 0) {
+            if (keys.length == 1) {
+                redisTemplate.delete(keys[0]);
+            } else {
+                redisTemplate.delete(Arrays.asList(keys));
+            }
+        }
+    }
+
+    /**
+     * String缓存获取
+     */
+    public Object get(String key) throws Exception {
+        return key == null ? null : redisTemplate.opsForValue().get(key);
+    }
+
+    /**
+     * String缓存存储
+     */
+    public void set(String key, Object value) throws Exception {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    /**
+     * String缓存存储限时
+     */
+    public void set(String key, Object value, Long time) throws Exception {
+        if (time < 0) {
+            throw new RuntimeException("time必须大于等于0");
+        }
+        redisTemplate.opsForValue().set(key, value, time, TimeUnit.SECONDS);
+    }
+
+    /**
+     * Map数据获取
+     */
+    public Object hget(String key, String item) throws Exception {
+        return redisTemplate.opsForHash().get(key, item);
+    }
+
+    /**
+     * Map数据存储
+     */
+    public void hset(String key, String item, Object value) throws Exception {
+        redisTemplate.opsForHash().put(key, item, value);
+    }
+
+    /**
+     * Map数据存储 expire
+     */
+    public void hset(String key, String item, Object value, Long time) throws Exception {
+        redisTemplate.opsForHash().put(key, item, value);
+        this.expire(key, time);
+    }
+
+    /**
+     * Map数据删除 key item
+     */
+    public void hdel(String key, String item) throws Exception {
+        redisTemplate.opsForHash().delete(key, item);
+    }
+
+    /**
+     * Map数据获取多个键值
+     */
+    public Map<Object, Object> hmget(String key) {
+        return redisTemplate.opsForHash().entries(key);
+    }
+
+    /**
+     * Map数据设置 key map
+     */
+    public void hmset(String key, Map<String,Object> map) {
+        redisTemplate.opsForHash().putAll(key, map);
+    }
+
+    /**
+     * Map数据设置 key map expire
+     */
+    public void hmset(String key, Map<String,Object> map, Long time) throws Exception {
+        if (time < 0) {
+            throw new RuntimeException("time必须大于等于0");
+        }
+        redisTemplate.opsForHash().putAll(key, map);
+        this.expire(key, time);
+    }
+
+    /**
+     * Map数据 has key
+     */
+    public Boolean hhasItem(String key, String item) throws Exception {
+        return redisTemplate.opsForHash().hasKey(key, item);
+    }
+
+}
